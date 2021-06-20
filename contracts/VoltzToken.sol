93bfdbd978e54335091c9f9ae2ba84d8d7c37302
Mayank Negi <mayank.negi0@gmail.com>
2021-06-20 22:59:35 +0530

    delegate token

--- contracts/VoltzToken.sol
+++ contracts/VoltzToken.sol
@@ -6,8 +6,10 @@ contract VoltzToken{
     string public standard = "Voltz Token v1.0";
     uint256 public totalSupply;
     mapping(address=>uint) public balanceOf;
+    mapping(address=>mapping(address=>uint)) public allowance;
 
     event Transfer(address indexed _from , address indexed _to,uint _value);
+    event Approval(address indexed _owner,address indexed _spender,uint _value);
 
     //constructor
     constructor(uint _initialSupply) public{
@@ -22,6 +24,27 @@ contract VoltzToken{
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Delegated Transfer
+
+    // Account 'A' approve Account "B" to transfer on its behalf 
+    function approve(address _spender,uint _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns(bool success){
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] -= _value; 
+
+        allowance[_from][msg.sender] -= _value;
+        
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 const VoltzToken = artifacts.require("VoltzToken");
 
 contract("VoltzToken",accounts=>{
@@ -49,6 +51,62 @@ contract("VoltzToken",accounts=>{
             return tokenInstance.transfer.call(accounts[1],25000,{from:accounts[0]});
         }).then(success=>{
             assert.equal(success,true,"successful transfer ")
+        })
+    })
+
+    it("Approves tokens for delegated transfer",()=>{
+        return VoltzToken.deployed().then((instance)=>{
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1],100);
+        }).then(success=>{
+            assert.equal(success,true);
+            return tokenInstance.approve(accounts[1],100);
+        }).then(receipt=>{
+            assert.equal(receipt.logs.length,1,"triggers one event");
+            assert.equal(receipt.logs[0].event,"Approval","Should be Approval Event");
+            assert.equal(receipt.logs[0].args._owner,accounts[0],"logs the account the token are authorised by");
+            assert.equal(receipt.logs[0].args._spender,accounts[1],"logs the account the token are authorised to");
+            assert.equal(receipt.logs[0].args._value,100,"logs the transfer amount");
+            return tokenInstance.allowance(accounts[0],accounts[1]);
+        }).then(allowance=>{
+            assert.equal(allowance.toNumber(),100,"stores the allowance for delegated transfer");
+        })
+    })
+
+    it("handles delegate token transfer",()=>{
+        return VoltzToken.deployed().then((instance)=>{
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+
+            return tokenInstance.transfer(fromAccount,100,{from:accounts[0]});
+        }).then(receipt=>{
+            return tokenInstance.approve(spendingAccount,10,{from:fromAccount})
+        }).then(receipt=>{
+            return tokenInstance.transferFrom(fromAccount,toAccount,9999,{from:spendingAccount})
+        }).then(assert.fail).catch(error=>{
+            assert(error.message.indexOf('revert')>=0,'cannot transfer value larger than balance')
+            return tokenInstance.transferFrom(fromAccount,toAccount,20,{from:spendingAccount})
+        }).then(assert.fail).catch(error=>{
+            assert(error.message.indexOf('revert')>=0,'cannot transfer value larger than approved amount')
+            return tokenInstance.transferFrom.call(fromAccount,toAccount,10,{from:spendingAccount});
+        }).then(success=>{
+            assert.equal(success,true);
+            return tokenInstance.transferFrom(fromAccount,toAccount,10,{from:spendingAccount});
+        }).then(receipt=>{
+            assert.equal(receipt.logs.length,1,"triggers one event");
+            assert.equal(receipt.logs[0].event,"Transfer","Should be Transfer Event");
+            assert.equal(receipt.logs[0].args._from,fromAccount,"logs the account the token are authorised by");
+            assert.equal(receipt.logs[0].args._to,toAccount,"logs the account the token are authorised to");
+            assert.equal(receipt.logs[0].args._value,10,"logs the transfer amount");
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(balance=>{
+            assert.equal(balance.toNumber(),90,"deducts the amount");
+            return tokenInstance.balanceOf(toAccount);
+        }).then(balance=>{
+            assert.equal(balance.toNumber(),10,"adds balance to the recieving account")
+            return tokenInstance.allowance(fromAccount,spendingAccount);
         })
     })
 })
