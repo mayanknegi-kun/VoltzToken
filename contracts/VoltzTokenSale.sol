82727ce1dd79eba89914b2a55913a4ff9750d7d1
Mayank Negi <mayank.negi0@gmail.com>
2021-06-21 16:57:46 +0530

    token sale

--- contracts/VoltzTokenSale.sol
+++ contracts/VoltzTokenSale.sol
@@ -4,9 +4,12 @@ import "./VoltzToken.sol";
 
 contract VoltzTokenSale{
 
-    address admin;
+    address payable admin;
     VoltzToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
+
+    event Sell(address _buyer,uint256 _amount);
 
     constructor(VoltzToken _tokenContract,uint _tokenPrice) public{
         //assign admin
@@ -15,6 +18,31 @@ contract VoltzTokenSale{
         tokenContract = _tokenContract;
         //assign Token Price
         tokenPrice = _tokenPrice;
+    }
+
+    //safe multiply function
+    function multiply(uint x,uint y) internal pure returns(uint z){
+        require(y==0 || (z=x*y)/y==x);
+    }
+
+    function buyToken(uint256 _numberOfToken) public payable {
+        require(msg.value == multiply(_numberOfToken,tokenPrice));
+        // require contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfToken);
+        // require that a transfer is succesful
+        require(tokenContract.transfer(msg.sender,_numberOfToken));
+        tokenSold += _numberOfToken; 
+        emit Sell(msg.sender,_numberOfToken);
+    }
+
+    //ending sale
+    function endSale() public {
+        //require admin
+        require(msg.sender==admin);
+        //send remaining token to admin
+        require(tokenContract.transfer(admin,tokenContract.balanceOf(address(this))));
+        //destroy contract
+        selfdestruct(admin);
     }
 
 }
--- test/TokenSale.js
+++ test/TokenSale.js
@@ -1,23 +1,85 @@
 const { assert } = require("chai");
 
 const VoltzTokenSale = artifacts.require("VoltzTokenSale");
+const VoltzToken = artifacts.require("VoltzToken");
 
-contract("VoltzTokenSale",()=>{
+contract("VoltzTokenSale",(accounts)=>{
     var tokenInstance;
+    var tokenSaleInstance;
     var tokenPrice = 100000000000000; //in wei
-
+    var admin = accounts[0];
+    var buyer = accounts[0];
+    var tokenAvailable = 750000; 
+    var numberOfTokens;
     it('initializes the contract with correct values',()=>{
         return VoltzTokenSale.deployed().then((instance)=>{
-            tokenInstance = instance;
-            return tokenInstance.address;
+            tokenSaleInstance = instance;
+            return tokenSaleInstance.address;
         }).then((address)=>{
             assert.notEqual(address,0x0,"has a contract address")
-            return tokenInstance.tokenContract()
+            return tokenSaleInstance.tokenContract()
         }).then(address=>{
             assert.notEqual(address,0x0);
-            return tokenInstance.tokenPrice();
+            return tokenSaleInstance.tokenPrice();
         }).then(price=>{
             assert.equal(price,tokenPrice,"token price is correct");
         })
     })
+
+    it("facilitates token buying",()=>{
+        return VoltzToken.deployed().then(instance=>{
+            tokenInstance = instance;
+            return tokenInstance.deployed();
+        }).then(instance=>{
+            tokenSaleInstance = instance;
+            //provision 75% of totalSupply
+            return tokenInstance.tansfer(tokenSaleInstance.address,tokenAvailable,{from:admin})
+        }).then(receipt=>{
+            numberOfTokens = 10;
+            return tokenSaleInstance.buyToken(numberOfTokens,{from:buyer,value:numberOfTokens*tokenPrice})
+        }).then(receipt=>{
+            assert.equal(receipt.logs.length,1,"triggers one event");
+            assert.equal(receipt.logs[0].event,"Sell","Should be Sell Event");
+            assert.equal(receipt.logs[0].args._buyer,buyer,"logs the account the token are authorised by");
+            assert.equal(receipt.logs[0].args._amount,numberOfTokens,"logs the transfer amount");
+            return tokenSaleInstance.tokenSold();
+        }).then(amount=>{
+            assert.equal(amount.toNumber(),numberOfTokens,"increment the number of token sold");
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        }).then(balance=>{
+            assert.equal(balance.toNumber(),tokenAvailable-numberOfTokens);
+            return tokenSaleInstance.buyToken(numberOfTokens,{from:buyer,value:1})            
+        }).then(assert.fail).catch(error=>{
+            assert(error.message.indexOf('revert')>=0,"msg.value must be equal number of token in wei")
+            return tokenSaleInstance.buyToken(800000, { from: buyer, value: numberOfTokens * tokenPrice })
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');
+          });
+
+    });
+
+    it('ends token sale', function() {
+        return VoltzToken.deployed().then(function(instance) {
+          // Grab token instance first
+          tokenInstance = instance;
+          return VoltzTokenSale.deployed();
+        }).then(function(instance) {
+          // Then grab token sale instance
+          tokenSaleInstance = instance;
+          // Try to end sale from account other than the admin
+          return tokenSaleInstance.endSale({ from: buyer });
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'must be admin to endSale');
+          return tokenSaleInstance.endSale({ from: admin });
+
+        })// End sale as admin
+        .then(function(receipt) {
+          return tokenInstance.balanceOf(admin);
+        }).then(function(balance) {
+          assert.equal(balance.toNumber(), 999990, 'returns all unsold Voltz tokens to admin');
+          // Check that the contract has no balance
+          balance = web3.eth.getBalance(tokenSaleInstance.address)
+          assert.equal(balance.toNumber(), 0);
+        });
+      });
 })
